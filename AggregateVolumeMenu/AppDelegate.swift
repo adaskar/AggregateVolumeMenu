43aff042917322fce8ad04315d247ec64acc3b6f
guru <gurhan.polat@gmail.com>
2020-12-21 18:57:12 +0300

    Initial Commit

---
+++ AggregateVolumeMenu/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AggregateVolumeMenu
+//
+//  Created by Gurhan Polat on 21.12.2020.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
